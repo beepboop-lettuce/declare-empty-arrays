577ce93af44772431608e5daa8a2016db1d3e9f8
beepboop-lettuce <ericaharris0486@gmail.com>
2022-03-01 13:29:54 -0600

    creating empty arrays cleaning up formatting

--- main.go
+++ main.go
@@ -3,11 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	names := [3]string{
-		"Rachel",
-		"Jen",
-		"Shavonda",
-	}
+	var names [3]string
 
-	fmt.Printf("names : %q\n", names)
+	var distances [5]int
+
+	var data [5]uint8
+
+	var ratios [1]float64
+
+	var alives [4]bool
+
+	var zero [0]uint8
+
+	fmt.Printf("names : %#v\n", names)
+	fmt.Printf("distances: %#v\n", distances)
+	fmt.Printf("data: %#v\n", data)
+	fmt.Printf("ratios: %#.2v\n", ratios)
+	fmt.Printf("alives: %#v\n", alives)
+	fmt.Printf("zero: %#v\n", zero)
+
 }
